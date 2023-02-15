d9821d0329c0f0676c9ece688cdc43b59c65afe8
maxwellmuchiri <muchirimaxwel@gmail.com>
2023-02-15 06:55:05 +0300

    complete

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+    def bark
+      puts "Woof!"
+    end
+  end
+
+  fido = Dog.new
+  fido.bark #=> "Woof!"
+
+  snoopy = Dog.new
+  snoopy.bark #=> "Woof!"
+
+  class Dog
+    def sit
+        puts "The Dog is sitting"
+    end
+end
